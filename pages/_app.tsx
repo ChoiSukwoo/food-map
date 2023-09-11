da42405de4e63fba3bf6c4d161d69b653cbbf116
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-09-11 21:32:07 +0900

    구글 애널릭티스 추가
    구글 애널릭티스 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from 'seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-RCJMN3SQX2');
+            `,
+        }}
+      />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-RCJMN3SQX2`} />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
