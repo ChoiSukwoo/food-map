615516f3fe0a82108dd3f04d20e5c9e45266e9dc
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2023-09-11 20:45:48 +0900

    네이버 도메인 소유 확인
    네이버 도메인 소유 확인

--- seo.config.js
+++ seo.config.js
@@ -1,8 +1,8 @@
 export default {
-  titleTemplate: '%s - Next.js 시작하기',
+  titleTemplate: '%s - 네이버 지도 샘플',
   openGraph: {
     type: 'website',
-    site_name: 'Next.js 시작하기',
+    site_name: '네이버 지도 샘플',
     images: [{ url: 'https://nextjs.org/static/blog/next-13/twitter-card.png' }],
   },
   additionalLinkTags: [
@@ -19,6 +19,12 @@ export default {
     {
       name: 'google-site-verification',
       content: 'O0r_20aU1JVk1sbI7E50r6RXiTRFrN8jG_a3uSeG4A0',
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: 'naver-site-verification',
+      content: '1f66c7f204d117f29a45ae50deaa7ae1b981b036',
     },
   ],
 };
